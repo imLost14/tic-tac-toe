2a75b79c288edc40d17145d9e0d45dd6455bb45a
imLost14 <hrdc.1407@hotmail.com>
2025-03-11 07:37:52 -0500

    terminando funcionalidad de ganador

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -4,38 +4,69 @@ import random
 def create_tablero():
     return [[str(3 * i + j + 1)for j in range(3)] for i in range(3)]
 
+
 def mostrar_tablero(tablero):
      for fila in tablero:
             print('----+---+---*')
             print('|', ' | '.join(fila), '|')
-            
-def marcar_tablero(tablero, numero, marca):
-	for i in range(len(tablero)):
-		for j in range(len(tablero)):
-			if (tablero[i][j] == numero):
-				tablero[i][j] = marca
-				return True
-	return False
 
-def  jugar_tic_tac_toe():
+
+def marcar_tablero(tablero, numero, marca):
+    for i in range(len(tablero)):
+        for j in range(len(tablero)):
+            if (tablero[i][j] == numero):
+                tablero[i][j] = marca
+                return True
+    return False
+
+
+def verificar_ganador(tablero):
+    # verificacion en filas y columnas
+    for i in range(3):
+        if tablero[i][0] == tablero[i][1] == tablero[i][2]:
+            return tablero[i][0]
+        if tablero[0][i] == tablero[1][i] == tablero[2][i]:
+            return tablero[0][i]
+        # verificacion en diagonal
+    if tablero[0][0] == tablero[1][1] == tablero[2][2]:
+        return tablero[0][0]
+    if tablero[0][2] == tablero[1][1] == tablero[2][0]:
+        return tablero[0][2]
+    return None
+
+
+def jugar_tic_tac_toe():
     print("Bienvenido a jugar tic tac toe")
     tablero = create_tablero()
     while True:
         mostrar_tablero(tablero)
-                
-        #Turno de usuario
+
+        # Turno de usuario
         while True:
-            numero = int(input("Ingrese un numero del 1 al 9 para marcar y 0 para salir: "))
+            numero = int(
+    input("Ingrese un numero del 1 al 9 para marcar y 0 para salir: "))
             if numero == 0:
                 print("Hasta pronto")
                 return False
-            if not 1 <= numero <= 9 or not marcar_tablero(tablero, str(numero), 'o'):
+            if not 1 <= numero <= 9 or not marcar_tablero(
+                tablero, str(numero), 'o'):
                 print("Ingrese un numero entre 1 y 9 o ya esta marcado")
                 continue
             marcar_tablero(tablero, str(numero), 'o')
             print("numero marcado")
-            #mostrar_tablero(tablero)
+            # mostrar_tablero(tablero)
             break
+        # Verificar si el usuario ganó
+        ganador = verificar_ganador(tablero)
+        if ganador:
+            if ganador == "o":
+                print("¡Felicidades, ha ganado el ususrio!")
+            elif ganador == "x":
+                print("La máquina ha ganado. ¡Mejor suerte la próxima vez!")
+            break
+
+
+
         #Turno de maquina
         while True:
             mov_maquina = random.randrange(1,10)
@@ -43,8 +74,18 @@ def  jugar_tic_tac_toe():
                 continue
             marcar_tablero(tablero, str(mov_maquina), "x")
             break
-            
+        # Verificar si el usuario ganó
+        ganador = verificar_ganador(tablero)
+        if ganador:
+            if ganador == "o":
+                print("¡Felicidades, ha ganado el usuario!")
+            elif ganador == "x":
+                print("La máquina ha ganado. ¡Mejor suerte la próxima vez!")
+            break
+
+
     mostrar_tablero(tablero)
+    
             
 jugar_tic_tac_toe()
             
